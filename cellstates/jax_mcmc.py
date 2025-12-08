7bb05e2ba59a40db551e8307b7376ee27590a187
Rémy Nicolle <remy.c.nicolle@gmail.com>
2025-12-08 23:26:58 +0100

    debug jaxmcmc

--- cellstates/jax_mcmc.py
+++ cellstates/jax_mcmc.py
@@ -147,7 +147,8 @@ def greedy_partition_sweep_jax(
         ll_chunk = ll_arr[idx]
         size_chunk = sizes_arr[idx]
 
-        ll_old_after = _ll_remove(counts_arr[:, c_old:c_old+1], cell_vec[:, None], lam_vec, B, lam_sum, sizes_arr[c_old])
+        old_slice = jax.lax.dynamic_slice(counts_arr, (0, c_old), (counts_arr.shape[0], 1))
+        ll_old_after = _ll_remove(old_slice, cell_vec[:, None], lam_vec, B, lam_sum, sizes_arr[c_old])
         ll_new = _ll_add(counts_chunk, cell_vec[:, None], lam_vec, B, lam_sum)
 
         # delta = new + old_after - old_before - cand_before
@@ -183,9 +184,8 @@ def greedy_partition_sweep_jax(
                 counts_chunk = counts_arr[:, idx]
                 ll_chunk = ll_arr[idx]
                 size_chunk = sizes_arr[idx]
-                ll_old_after = _ll_remove(
-                    counts_arr[:, c_old:c_old+1], cell_vec[:, None], lam_vec, B, lam_sum, sizes_arr[c_old]
-                )
+                old_slice = jax.lax.dynamic_slice(counts_arr, (0, c_old), (counts_arr.shape[0], 1))
+                ll_old_after = _ll_remove(old_slice, cell_vec[:, None], lam_vec, B, lam_sum, sizes_arr[c_old])
                 ll_new = _ll_add(counts_chunk, cell_vec[:, None], lam_vec, B, lam_sum)
                 delta = ll_new + ll_old_after - ll_arr[c_old] - ll_chunk
                 delta = jnp.where(idx == c_old, -jnp.inf, delta)
